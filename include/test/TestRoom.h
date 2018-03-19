1bce61914f64275fc7e8cb7a0a00c70b9d2790ac
Godron629 <gideon.richter@uleth.ca>
2018-03-18 20:26:13 -0600

    Finish room tests

--- include/test/TestRoom.h
+++ include/test/TestRoom.h
@@ -1,9 +1,9 @@
 #ifndef TESTROOM_H
 #define TESTROOM_H
 
-
 #include "Room.h"
 #include <cppunit/extensions/HelperMacros.h>
+#include <string>
 
 
 class TestRoom : public CppUnit::TestFixture {
@@ -12,12 +12,6 @@ class TestRoom : public CppUnit::TestFixture {
 
     CPPUNIT_TEST(test_get_id);
     CPPUNIT_TEST(test_get_name);
-    CPPUNIT_TEST(test_get_neighbour_n);
-    CPPUNIT_TEST(test_get_neighbour_s);
-    CPPUNIT_TEST(test_get_neighbour_w);
-    CPPUNIT_TEST(test_get_neighbour_e);
-    CPPUNIT_TEST(test_get_neighbour_u);
-    CPPUNIT_TEST(test_get_neighbour_d);
     CPPUNIT_TEST(test_get_items);
     CPPUNIT_TEST(test_get_npcs);
     CPPUNIT_TEST(test_add_item);
@@ -27,19 +21,23 @@ class TestRoom : public CppUnit::TestFixture {
 
     public:
 
+        void setUp();
+
         void test_get_id();
         void test_get_name();
-        void test_get_neighbour_n();
-        void test_get_neighbour_s();
-        void test_get_neighbour_e();
-        void test_get_neighbour_w();
-        void test_get_neighbour_u();
-        void test_get_neighbour_d();
         void test_get_npcs();
         void test_get_items();
         void test_delete_item();
         void test_add_item();
 
+    protected:
+        Room* m_room;
+        std::string m_id;
+        std::string m_name;
+        std::string m_desc;
+        tNeighbours m_neighbours;
+        tItemList m_items;
+        tNpcList m_npcs;
 };
 
 #endif // TESTROOM_H
--- test/TestRoom.cpp
+++ test/TestRoom.cpp
@@ -11,27 +11,60 @@ using std::vector;
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestRoom);
 
+void TestRoom::setUp() {
+    m_name = "test_name";
+    m_id = "test_id";
+    m_desc = "test_desc";
+
+    m_neighbours = {
+        {"n", "1"},
+        {"e", "2"},
+        {"s", "3"},
+        {"w", "4"},
+        {"u", "5"},
+        {"d", "6"}};
+
+    m_npcs.push_back(new Scientist());
+
+    m_items.push_back(new Helmet());
+
+    m_room = new Room(m_id, m_name, m_desc, m_neighbours, m_npcs, m_items);
+}
+
 void TestRoom::test_get_id() {
-    tNeighbours neighbours = {{"n", "z"}};;
-    tNpcList npcs;
-    npcs.push_back(new Scientist());
-
-    tItemList items;
-    items.push_back(new Helmet());
-
-    string result = Room("1", "name", "description", neighbours, npcs, items).getId();
-    string expectedResult = "2";
+    string expectedResult = "test_id";
+    string result = m_room->getId();
     CPPUNIT_ASSERT_EQUAL(result, expectedResult);
 }
 
-void TestRoom::test_get_name() {};
-void TestRoom::test_get_neighbour_n() {};
-void TestRoom::test_get_neighbour_s() {};
-void TestRoom::test_get_neighbour_e() {};
-void TestRoom::test_get_neighbour_w() {};
-void TestRoom::test_get_neighbour_u() {};
-void TestRoom::test_get_neighbour_d() {};
-void TestRoom::test_get_npcs() {};
-void TestRoom::test_get_items() {};
-void TestRoom::test_delete_item() {};
-void TestRoom::test_add_item() {};
+void TestRoom::test_get_name() {
+    string expectedResult = "test_name";
+    string result = m_room->getName();
+    CPPUNIT_ASSERT_EQUAL(result, expectedResult);
+};
+
+void TestRoom::test_get_npcs() {
+    int expectedLength = 1;
+    int npcsLength = m_room->getNpcs().size();
+    CPPUNIT_ASSERT_EQUAL(npcsLength, expectedLength);
+};
+
+void TestRoom::test_get_items() {
+    int expectedLength = 1;
+    int itemsLength = m_room->getItems().size();
+    CPPUNIT_ASSERT_EQUAL(itemsLength, expectedLength);
+};
+
+void TestRoom::test_delete_item() {
+    m_room->removeItem(new Helmet);
+    int expectedLength = 0;
+    int itemsLength = m_room->getItems().size();
+    CPPUNIT_ASSERT_EQUAL(itemsLength, expectedLength);
+};
+
+void TestRoom::test_add_item() {
+    m_room->addItem(new Helmet);
+    int expectedLength = 2;
+    int itemsLength = m_room->getItems().size();
+    CPPUNIT_ASSERT_EQUAL(itemsLength, expectedLength);
+};
